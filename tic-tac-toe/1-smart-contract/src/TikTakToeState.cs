fdf46a66fa7646cf870361ce3c394f2da8b949ea
RutvikGhaskataEalf <rutikbhai.ghaskata@aelf.io>
2024-09-02 11:13:23 +0530

    Integrated tic-tac-toe smart contract

---
+++ tic-tac-toe/1-smart-contract/src/TikTakToeState.cs
@@ -0,0 +1,17 @@
+using AElf.Sdk.CSharp.State;
+using AElf.Types;
+
+namespace AElf.Contracts.TikTacToe
+{
+    // The state class is access the blockchain state
+    public partial class TikTacToeState : ContractState 
+    {
+        // A state to check if contract is initialized
+        public BoolState Initialized { get; set; }
+        public SingletonState<Address> Owner { get; set; }
+        public StringState Board { get; set; } // Board state as a concatenated string
+        public StringState CurrentPlayer { get; set; } // X or O
+        public StringState GameStatus { get; set; } // ongoing, finished, draw
+        public StringState Winner { get; set; } // X or O
+    }
+}
